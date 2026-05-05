8422c6e77d0f1f4fab02cbd744faed203cd8c0d3
FaresAlkhier1 <faalkhier@ctemc.org>
2026-05-05 09:28:41 -0400

    Update branchExample1.py

--- branchExample1.py
+++ branchExample1.py
@@ -1,12 +1,1 @@
-"""
-Name: branchExample.py
-Authors: Fares Alkhier and Sam Thakker
-Purpose: Demonstrate GitHub branches
-Date: 5-5-26
-"""
 
-counter = 0
-
-while(counter < 10):
-  print("Counter: " + str(counter))
-  counter +=1
