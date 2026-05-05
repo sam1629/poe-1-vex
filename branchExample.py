21cf7ce268774235c40415d94f179b2ac6980c3c
sam1629 <samthakker13@gmail.com>
2026-05-05 09:21:56 -0400

    Create branchExample.py

---
+++ branchExample.py
@@ -0,0 +1,12 @@
+"""
+Name: branchExample.py
+Author: Sam Thakker and Fares Alkhier
+Purpose: Demonstrate GitHub branches
+Date: 5-5-26
+"""
+
+counter = 0
+
+while(counter < 10):
+    print("Counter: " + str(counter))
+    counter += 1
