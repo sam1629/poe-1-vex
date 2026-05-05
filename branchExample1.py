6db024b19ab195c1ab1edc68cc600fc5d1144442
FaresAlkhier1 <faalkhier@ctemc.org>
2026-05-05 09:27:23 -0400

    Create branchExample1.py

---
+++ branchExample1.py
@@ -0,0 +1,12 @@
+"""
+Name: branchExample.py
+Authors: Fares Alkhier and Sam Thakker
+Purpose: Demonstrate GitHub branches
+Date: 5-5-26
+"""
+
+counter = 0
+
+while(counter < 10):
+  print("Counter: " + str(counter))
+  counter +=1
