d167ccdf8606075ad5e6822aaafb948ab99e3aab
sam1629 <samthakker13@gmail.com>
2026-05-06 07:37:03 -0400

    new

---
+++ Thakker_Alhkier-vex/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       samear.taker                                                 #
+# 	Created:      5/5/2026, 9:38:30 AM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
